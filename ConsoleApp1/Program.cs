496df09eb09918a16f73054701b033012fa0a260
marina kanjuga <marinakanjuga@gmail.com>
2019-11-23 22:28:31 +0100

    vezba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,18 +6,51 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string operacija, broj1, broj2;
+            int num1, num2;
+            broj1 = Console.ReadLine();           
+            broj2 = Console.ReadLine();
             
-            Console.WriteLine(Go(5));
-            Console.ReadKey();
-        }
-        public static string Go(int num)
-        {
-            string dash = "";
-            for (int i = 0; i < num; i++)
+            operacija = Console.ReadLine();
+            if (operacija != "" || broj1 != "" || broj2 !="")
             {
-                dash = dash + "-";
+                num1 = Convert.ToInt32(broj1);
+                num2 = Convert.ToInt32(broj2);
+                int rez = rezultat(num1, num2, operacija);
+                Console.WriteLine("{0}, {1}, {2} = {3}", num1, num2, operacija, rez);
             }
-            return dash;
+            else 
+            {
+                num1 = 2;
+                num2 = 2;
+                int rez = rezultat(num1, num2, operacija);
+                Console.WriteLine("{0}, {1}, {2} = {3}", num1, num2, operacija, rez);
+            }
+          
+            static int rezultat (int num1, int num2, string operacija)
+            {
+                int k = 0;
+                if (operacija == "+")
+                {
+                    k = num1 + num2;
+                }
+                else if (operacija == "-")
+                {
+                    k = num1 - num2;
+                }
+                else if (operacija == "*")
+                {
+                    k = num1 * num2;
+                }
+                else if (operacija == "/")
+                {
+                    k = num1 / num2;
+                }
+                return k;
+            }
+            Console.ReadKey();
+       
         }
-    }
-}
+    }  
+ }
+
