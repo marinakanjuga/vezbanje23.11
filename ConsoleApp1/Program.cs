476c226a785b2c257046cb06d1854dfda93b6de2
marina kanjuga <marinakanjuga@gmail.com>
2019-11-23 10:50:43 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Go(5));
+            Console.ReadKey();
+        }
+        public static string Go(int num)
+        {
+            string dash = "";
+            for (int i = 0; i < num; i++)
+            {
+                dash = dash + "-";
+            }
+            return dash;
+        }
+    }
+}
