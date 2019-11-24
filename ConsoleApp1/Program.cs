e8430cc9502bc9118094fff3383adca0e600f6dc
marina kanjuga <marinakanjuga@gmail.com>
2019-11-24 16:07:07 +0100

    zadatk sa nizom stringa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,38 +5,46 @@ using System.Linq;
 namespace ConsoleApp1
 {
     class Program
-    {//napisati fun koja ce kao ulazni parametar imati niz
-        //vratiti razliku izmedju najmanjeg i najveceg broja
+    {//fun koja ce kao ulazni par imati niz stringova. 
+        //fun treba da vrati duz najduzeg stringa, u klasi strings
         static void Main(string[] args)
         {
-            najveciBroj();
+            najduziString();
             Console.ReadKey();
 
         }
-        static void najveciBroj()
+        static void najduziString()
         {
-            List<int> brojevi = new List<int>();
+            List<string> stringovi = new List<string>();
             string a;
             int n;
             do
             {
-                Console.WriteLine("upisi broj");
-                a = Console.ReadLine();
+                Console.WriteLine("upisi string");
+                a = Console.ReadLine();             
                 if (a == "")
                 {
-                    break;                  
-                }
+                    break;
+                }               
                 n = Convert.ToInt32(a);
-                brojevi.Add(n);
+                stringovi.Add(a);
+                Console.WriteLine(suma(n));
             }
             while (true);
-            int rez1 = brojevi.Max();
-            int rez2 = brojevi.Min();
-            int rezultat;
-            rezultat = rez1 - rez2;
-            Console.WriteLine("rezultat je {0} - {1} = {2}", rez1, rez2, rezultat);
+            //Console.WriteLine("najduzi string je {0}", stringovi.Max());
         }
 
+        static int suma (int n) 
+        {
+            string m = Convert.ToString(n);
+            int suma = 0;
+            for (int i = 0; i < m.Length; i++)
+            {
+                suma = suma + Convert.ToInt32(m.Substring(i, 1));
+            }
+            return suma;
+
+        }
 
 
 
