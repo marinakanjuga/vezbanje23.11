e49093352cdc8d8301afc722d5165e9a397571eb
marina kanjuga <marinakanjuga@gmail.com>
2019-11-25 22:16:25 +0100

    zadatak 3

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class StringFunctions
+    {
+        public void CountWords () 
+        {
+            string a;            
+            a = Console.ReadLine();
+            string[] splitovano = a.Split(new char[] { ' ' }, StringSplitOptions.None);
+            Console.WriteLine(splitovano.Length);
+            Console.ReadKey();
+        }
+        public void CountSpaces()
+        {
+            string a;
+            a = Console.ReadLine();
+            string[] splitovano = a.Split(new char[] { ' ' }, StringSplitOptions.None);
+            Console.WriteLine(splitovano.Length-1);
+            Console.ReadKey();
+        }
+        public void ShortestWord()
+        {
+            string min = " ";
+            string a;
+            a = Console.ReadLine();
+            string[] splitovano = a.Split(new char[] { ' ' }, StringSplitOptions.None);
+            for (int i = 0; i < splitovano.Length; i++)  
+            {
+                int lent = splitovano[i].Length;
+                if (lent < min.Length) 
+                {
+                    min = splitovano[i];
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+        public void LongestWord()
+        {
+            string max = " ";
+            string a;
+            a = Console.ReadLine();
+            string[] splitovano = a.Split(new char[] { ' ' }, StringSplitOptions.None);
+            for (int i = 0; i < splitovano.Length; i++)
+            {
+                int lent = splitovano[i].Length;
+                if (lent > max.Length)
+                {
+                    max = splitovano[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,56 +9,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+          
+
+            StringFunctions funkcija = new StringFunctions();
             
+            Console.WriteLine("uensite tekst");
+            funkcija.CountWords();
+            funkcija.CountSpaces();
+            funkcija.LongestWord();
+            funkcija.ShortestWord();
 
 
-            Console.WriteLine("unesite brojeve");
-            List<int> brojevi = new List<int>();
-            int a;
-            string b;
-            do
-            {
-                b = Console.ReadLine();
-                a = Convert.ToInt32(b);
-                if (a < 0)
-                {
-                    break;
-                }
-                brojevi.Add(a);
-            } while (true);
-
-            Console.WriteLine(SumaUnetihBrojeva(brojevi));
-            Console.WriteLine(AritmetickaSredina(brojevi));
-            SortiranaLista(brojevi);
-            Console.ReadKey();
-        }
-
-        static int SumaUnetihBrojeva (List<int> brojevi) 
-        {
-            int sum = 0;
-            foreach (int i in brojevi) 
-            {
-                sum += i;
-            }
-            return sum;
-        }
-        static int AritmetickaSredina (List<int> brojevi)
-        {
-            //double r;
-            int rez = 0;
-            double rez1 = Convert.ToDouble(rez);
-            rez1 = brojevi.Average();
-            return rez;          
-        }
-
-        static void SortiranaLista(List<int> brojevi)
-        {                       
-            brojevi.Sort();
-            foreach (int broj in brojevi) 
-            {
-                Console.WriteLine(broj);
-            }
+          
             
+          
+          
+           
+
 
         }
     }
