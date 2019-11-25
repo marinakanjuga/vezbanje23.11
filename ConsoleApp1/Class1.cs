63645794d31a9ae31ee6cf64988583eeca9f4646
marina kanjuga <marinakanjuga@gmail.com>
2019-11-25 20:29:31 +0100

    1.zadatak

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -7,36 +7,36 @@ namespace ConsoleApp1
 {
     class NovaKlasa
     {
-        public string color = "red"; 
-        public static string logFileName = "C:\\proba.log";
+        //public string color = "red"; 
+        //public static string logFileName = "C:\\proba.log";
 
-        
-        public static void log(string logMessage)
-        {
-            using (StreamWriter file = new StreamWriter(logFileName, true))
-            {
-                file.WriteLine(DateTime.Now.ToString("yyyy-mm-dd HH-mm-ss K"));
-                file.WriteLine(logMessage);
-                file.WriteLine("********");
-                file.WriteLine();
-            }
-        }
 
-        public static void nizovi () 
-        {
-            Console.WriteLine("unesite broj redova");
-            int a;
-            a = Convert.ToInt32(Console.ReadLine());
-            int b = 1;
-            for (int i = 1; i <= a; i++)
-            {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("{0}", b);
-                    b++;
-                }
-                Console.WriteLine();
-            }
-        }
+        //public static void log(string logMessage)
+        //{
+        //    using (StreamWriter file = new StreamWriter(logFileName, true))
+        //    {
+        //        file.WriteLine(DateTime.Now.ToString("yyyy-mm-dd HH-mm-ss K"));
+        //        file.WriteLine(logMessage);
+        //        file.WriteLine("********");
+        //        file.WriteLine();
+        //    }
+        //}
+
+        //public static void nizovi () 
+        //{
+        //    Console.WriteLine("unesite broj redova");
+        //    int a;
+        //    a = Convert.ToInt32(Console.ReadLine());
+        //        int b = 1;
+        //        for (int i = 1; i <= a; i++)
+        //        {
+        //            for (int j = 1; j <= i; j++)
+        //            {
+        //                Console.Write("{0}", b);
+        //                b++;
+        //            }
+        //            Console.WriteLine();
+        //        }
+        //    }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,82 +9,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            //NovaKlasa klasa = new NovaKlasa();
-            //NovaKlasa.nizovi();
-            //string[] cars = new string[] {"volvo", "opel", "bmv"};
+            
+            //1.zadatak
 
-            //NizBrojeva(new int[] { 1, 2, 3, 5, 6, 8 });
-            //int[] niz = { 1, 2, 3, 5, 6 };
-            //onsole.WriteLine(niz.Max());
-            //List<int> brojevi = new List<int>();
-            //Console.WriteLine(Spejsovi(new string[] {"maja", "kakak", "ksla"}));
-            string op, broj1, broj2;
-            int num1, num2;           
-            do
+            Console.WriteLine("unesite dva broja");
+            double a;
+            double b;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            if (a%2==0 && b%2==0) 
             {
-                broj1 = Console.ReadLine();
-                broj2 = Console.ReadLine();
-                op = Console.ReadLine();
-                if (op == "" || broj1 == "" || broj2 == "")
-                {
-                    break;
-                }
-                num1 = Convert.ToInt32(broj1);
-                num2 = Convert.ToInt32(broj2);
-                int rez = rezultat(num1, num2, op);
-                Console.WriteLine("{0}, {1}, {2} = {3}", num1, num2, op, rez);
-                
-            } while (true);
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-        
-            static int rezultat(int num1, int num2, string op)
-            {
-                int k = 0;
-                if (op == "+")
-                {
-                    k = num1 + num2;
-                }
-                else if (op == "-")
-                {
-                    k = num1 - num2;
-                }
-                else if (op == "*")
-                {
-                    k = num1 * num2;
-                }
-                else if (op == "/")
-                {
-                    k = num1 / num2;
-                }
-                return k;
+                double rezultat = a * b;
+                Console.WriteLine("rezultat je {0}", rezultat);
             }
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            else if (!(a % 2 == 0) && !(b % 2 == 0)) 
+            {
+                double rezultat = a + b;
+                Console.WriteLine("rezultat je {0}", rezultat);
+            }
+            else if ((!(a % 2 == 0) && b % 2 == 0) || (a % 2 == 0 && !(b % 2 == 0)))
+            {
+                double rezultat;
+                if (a>b)
+                {
+                    rezultat = a / b;
+                }
+                else 
+                {
+                    rezultat = b / a;
+                }
+                Console.WriteLine("rezultat je {0}", rezultat);
+            }
         }
-
+    }
 }
 
